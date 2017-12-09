cc5b516a20157a4e532a87b52df27b56c1d9d6e1
metrue <h.minghe@gmail.com>
2017-12-09 11:01:22 +0800

    fx version v0.0.2

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -1,9 +1,9 @@
 class Fx < Formula
   desc "fx, a simple but powerful Function as a Service build tools"
   homepage "https://github.com/metrue/fx"
-  url "https://github.com/metrue/fx/releases/download/v0.0.1/fx_0.0.1_macOS_64-bit.tar.gz"
-  version "0.0.1"
-  sha256 "c083de71161e82d0bf6daa0448c6f9a7cb17b6ea124c6ecd9cf9d73881894aca"
+  url "https://github.com/metrue/fx/releases/download/v0.0.2/fx_0.0.2_macOS_64-bit.tar.gz"
+  version "0.0.2"
+  sha256 "5b6ea141a67158118583e82682220e917ddc55cdd2d37c32238d6f45bbd32da6"
 
   def install
     bin.install "fx"
