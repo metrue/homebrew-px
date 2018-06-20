64bb13d7f945702cf518ac4e498da695044239cc
metrue <h.minghe@gmail.com>
2018-06-20 16:45:59 +0800

    Brew formula update for fx version vmaster-d73afe8

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -1,9 +1,9 @@
 class Fx < Formula
   desc "fx, a simple but powerful Function as a Service build tools"
   homepage "https://github.com/metrue/fx"
-  url "https://github.com/metrue/fx/releases/download/vmaster-950c65d2c683d4b465f5f230601b4ace86600e78/fx_master-950c65d2c683d4b465f5f230601b4ace86600e78_macOS_64-bit.tar.gz"
-  version "master-950c65d2c683d4b465f5f230601b4ace86600e78"
-  sha256 "c053ca7aa9fad3a12e16fdc3d16fc2d675dc157c7bd7489989eb6ecc249f5a3f"
+  url "https://github.com/metrue/fx/releases/download/vmaster-d73afe8/fx_master-d73afe8_macOS_64-bit.tar.gz"
+  version "master-d73afe8"
+  sha256 "cfae63dd83bee9e7f63752fe8e4e8163cd273d2f62983baa40da7dee58e1d53d"
 
   def install
     bin.install "fx"
