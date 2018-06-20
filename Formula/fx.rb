d9a9d3ec027e2f3dd956a3ebc99cf8ed0a05a0e3
metrue <h.minghe@gmail.com>
2018-06-20 16:21:25 +0800

    Brew formula update for fx version vci--autodeploy-27437a104a2616f41c75f9e6bb9d94e24bc771d6

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -1,15 +1,11 @@
 class Fx < Formula
   desc "fx, a simple but powerful Function as a Service build tools"
   homepage "https://github.com/metrue/fx"
-  url "https://github.com/metrue/fx/releases/download/v0.0.3/fx_0.0.3_macOS_64-bit.tar.gz"
-  version "0.0.3"
-  sha256 "a3c108d80d99dfda7a94fadcfb72a6e516518dee322e17960561ebe7486cc206"
+  url "https://github.com/metrue/fx/releases/download/vci--autodeploy-27437a104a2616f41c75f9e6bb9d94e24bc771d6/fx_ci--autodeploy-27437a104a2616f41c75f9e6bb9d94e24bc771d6_macOS_64-bit.tar.gz"
+  version "ci--autodeploy-27437a104a2616f41c75f9e6bb9d94e24bc771d6"
+  sha256 "9801eff276de24368a3444a062460446b522f682deb492c537d7c38dfeff3685"
 
   def install
     bin.install "fx"
-  end
-
-  test do
-    
   end
 end
