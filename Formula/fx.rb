576953612f7ffc5fe3ee68c94043642ef5a5dafe
metrue <h.minghe@gmail.com>
2018-07-28 15:54:56 +0800

    Brew formula update for fx version 0.2.1

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -1,9 +1,9 @@
 class Fx < Formula
   desc "fx, a simple but powerful Function as a Service build tools"
   homepage "https://github.com/metrue/fx"
-  url "https://github.com/metrue/fx/releases/download/0.2.0/fx_0.2.0_macOS_64-bit.tar.gz"
-  version "0.2.0"
-  sha256 "49dde1f0918da417fd96f3bae2bb2781056639ccc373398253c5c79af69df6a5"
+  url "https://github.com/metrue/fx/releases/download/0.2.1/fx_0.2.1_macOS_64-bit.tar.gz"
+  version "0.2.1"
+  sha256 "434bf61bc8a06818d67b5c1907bda635a90d8a5b8803f1aef8afe18d2be7be44"
 
   def install
     bin.install "fx"
