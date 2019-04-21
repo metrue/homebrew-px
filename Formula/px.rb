869ccd5f568685a5dccbaf57b0f43740b5dc7ea3
metrue <h.minghe@gmail.com>
2019-04-21 18:05:21 +0800

    px version v0.6.4

--- Formula/px.rb
+++ Formula/px.rb
@@ -1,9 +1,9 @@
 class Px < Formula
   desc ""
   homepage ""
-  url "https://github.com/metrue/px/releases/download/v0.0.63/px_0.0.63_darwin_amd64.tar.gz"
-  version "0.0.63"
-  sha256 "29dad519965b8c7c9c162401b1a40f247411eb14bd83c2f365aba0e337563330"
+  url "https://github.com/metrue/px/releases/download/v0.6.4/px_0.6.4_darwin_amd64.tar.gz"
+  version "0.6.4"
+  sha256 "bcc0e299901fbf2c1c108c606ac00c083bcef160875ae160acb06390327da53d"
 
   def install
     bin.install "px"
